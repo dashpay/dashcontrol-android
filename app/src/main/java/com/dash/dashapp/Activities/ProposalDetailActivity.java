a5816dd80f9fbf52d9c9a14992087d16ea54df89
LAPTOP-7Q8SI1F9\sebas <sebastien.ferrand.dev@gmail.com>
2017-09-16 20:12:43 +0700

    Proposal UI created

--- app/src/main/java/com/dash/dashapp/Activities/ProposalDetailActivity.java
+++ app/src/main/java/com/dash/dashapp/Activities/ProposalDetailActivity.java
@@ -7,28 +7,22 @@ import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
+import com.dash.dashapp.Model.Proposal;
 import com.dash.dashapp.R;
 
 public class ProposalDetailActivity extends AppCompatActivity {
 
-    private static final String CONTENT_RSS = "content_rss";
-    private TextView rss_content_view;
+    private static final String CONTENT_PROPOSAL = "proposal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_content_rss);
+        setContentView(R.layout.activity_content_proposal);
 
         Intent intent = getIntent();
-        String contentString = intent.getStringExtra(CONTENT_RSS);
+        Proposal proposal = (Proposal) intent.getSerializableExtra("CONTENT_PROPOSAL");
 
-        rss_content_view = (TextView) findViewById(R.id.content_view);
-        rss_content_view.setMovementMethod(LinkMovementMethod.getInstance());
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            rss_content_view.setText(Html.fromHtml(contentString,Html.FROM_HTML_MODE_LEGACY));
-        } else {
-            rss_content_view.setText(Html.fromHtml(contentString));
-        }
+
     }
 }
