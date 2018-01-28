f22bd9c4282cda39726acd7b9af65d4bb6013d7a
LAPTOP-7Q8SI1F9\sebas <sebastien.ferrand.dev@gmail.com>
2018-01-28 11:58:47 +0100

    Json parser okay, but server sometimes send empty jsonArray

--- app/src/main/java/com/dash/dashapp/application/DashControlApplication.java
+++ app/src/main/java/com/dash/dashapp/application/DashControlApplication.java
@@ -20,7 +20,6 @@ import org.json.JSONObject;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -57,7 +56,6 @@ public class DashControlApplication extends Application {
 
         importChartData();
     }
-
 
 
     private void pickDefaultLanguage() {
@@ -98,56 +96,114 @@ public class DashControlApplication extends Application {
         String marketString = "&market=DASH_USDT";
         String exchangeString = "&exchange=poloniex";
 
-        String URLGraph = URLs.URL_GRAPH + startDateString + endDateString + marketString + exchangeString;
+        // DEBUG NO limit
+        String noLimit = "&noLimit";
+
+        String URLGraph = URLs.URL_GRAPH + startDateString + endDateString + marketString + exchangeString + noLimit;
 
 
         Log.d(TAG, "Retrieving price data : " + URLGraph);
 
-        // Getting exchanges (default exchange to display)
-        JsonObjectRequest jsObjRequestExchanges = new JsonObjectRequest
-                (Request.Method.GET, URLGraph, null, new Response.Listener<JSONObject>() {
+        // For debug purpose when server does not send message back
+        String jsonString = "[{\"time\":\"2018-01-27T07:50:00.000Z\",\"close\":761.69261776,\"high\":761.69261776,\"low\":761.69261776,\"open\":761.69261776,\"pairVolume\":143.36117623,\"trades\":9,\"volume\":0.18821395000000002},{\"time\":\"2018-01-27T07:55:00.000Z\",\"close\":761.69262112,\"high\":768.14289846,\"low\":761.69262112,\"open\":768.14289845,\"pairVolume\":17.98223758,\"trades\":3,\"volume\":0.02357443},{\"time\":\"2018-01-27T08:00:00.000Z\",\"close\":761.99929985,\"high\":768.1392305,\"low\":761.69262175,\"open\":768.1392305,\"pairVolume\":103.09073902000002,\"trades\":4,\"volume\":0.13441663},{\"time\":\"2018-01-27T08:05:00.000Z\",\"close\":767.99910088,\"high\":767.99910089,\"low\":767.99910079,\"open\":767.99910079,\"pairVolume\":673.2444162199998,\"trades\":12,\"volume\":0.87662136},{\"time\":\"2018-01-27T08:10:00.000Z\",\"close\":768.20831676,\"high\":768.20831676,\"low\":767.99910079,\"open\":767.99910079,\"pairVolume\":1488.0962323700003,\"trades\":22,\"volume\":1.93728224},{\"time\":\"2018-01-27T08:15:00.000Z\",\"close\":771.55690987,\"high\":771.81320907,\"low\":768.14037699,\"open\":771.64135596,\"pairVolume\":2614.4459242499997,\"trades\":14,\"volume\":3.39329044},{\"time\":\"2018-01-27T08:20:00.000Z\",\"close\":773.55060698,\"high\":773.55060698,\"low\":767.99910079,\"open\":768.140377,\"pairVolume\":1579.34279617,\"trades\":5,\"volume\":2.0563814800000006},{\"time\":\"2018-01-27T08:25:00.000Z\",\"close\":766.67190014,\"high\":773.54171467,\"low\":766.67190014,\"open\":767.99910079,\"pairVolume\":217.57885602,\"trades\":4,\"volume\":0.28137722000000004},{\"time\":\"2018-01-27T08:30:00.000Z\",\"close\":764.29967512,\"high\":770,\"low\":764.29967512,\"open\":770,\"pairVolume\":219.30360239,\"trades\":5,\"volume\":0.28500758000000004},{\"time\":\"2018-01-27T08:35:00.000Z\",\"close\":764.1591053,\"high\":764.1591053,\"low\":764.1591053,\"open\":764.1591053,\"pairVolume\":44.33664882,\"trades\":2,\"volume\":0.05802018},{\"time\":\"2018-01-27T08:40:00.000Z\",\"close\":763.59906687,\"high\":763.59906687,\"low\":763.59906687,\"open\":763.59906687,\"pairVolume\":9.33099733,\"trades\":1,\"volume\":0.01221976},{\"time\":\"2018-01-27T08:45:00.000Z\",\"close\":761.99929983,\"high\":763.59906685,\"low\":761.99929983,\"open\":763.59906685,\"pairVolume\":1798.6542735599996,\"trades\":8,\"volume\":2.35877941}]";
 
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
+        try {
+            JSONArray jsonarray = new JSONArray(jsonString);
 
-                            // ...
-                            JSONArray json = new JSONArray(response);
-                            // ...
-                            Log.d(TAG, "Json response : " + json.toString());
+            for (int i = 0; i < jsonarray.length(); i++) {
 
-                            for (int i = 0; i < json.length(); i++) {
-                                HashMap<String, String> map = new HashMap<String, String>();
-                                JSONObject e = json.getJSONObject(i);
+                JSONObject jsonobj = null;
+                jsonobj = jsonarray.getJSONObject(i);
+
+                Log.d(TAG, "time : " + jsonobj.getString("time"));
+                Log.d(TAG, "close : " + jsonobj.getInt("close"));
+                Log.d(TAG, "high : " + jsonobj.getInt("high"));
+                Log.d(TAG, "low : " + jsonobj.getInt("low"));
+                Log.d(TAG, "open : " + jsonobj.getInt("open"));
+                Log.d(TAG, "pairVolume : " + jsonobj.getInt("pairVolume"));
+                Log.d(TAG, "trades : " + jsonobj.getInt("trades"));
+                Log.d(TAG, "volume : " + jsonobj.getInt("volume"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
-                                Log.d(TAG, "time : " + e.getString("time"));
-                                Log.d(TAG, "close : " + e.getString("close"));
-                                Log.d(TAG, "high : " + e.getString("high"));
-                                Log.d(TAG, "low : " + e.getString("low"));
-                                Log.d(TAG, "open : " + e.getString("open"));
-                                Log.d(TAG, "pairVolume : " + e.getString("pairVolume"));
-                                Log.d(TAG, "trades : " + e.getString("trades"));
-                                Log.d(TAG, "volume : " + e.getString("volume"));
-                            }
+    // Getting exchanges (default exchange to display)
+    JsonObjectRequest jsObjRequestExchanges = new JsonObjectRequest
+            (Request.Method.GET, URLGraph, null, new Response.Listener<JSONObject>() {
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+
+                        JSONArray jsonarray = new JSONArray(response);
+
+                        for (int i = 0; i < jsonarray.length(); i++) {
+
+                            JSONObject jsonobj = jsonarray.getJSONObject(i);
+
+                            Log.d(TAG, "time : " + jsonobj.getString("time"));
+                            Log.d(TAG, "close : " + jsonobj.getInt("close"));
+                            Log.d(TAG, "high : " + jsonobj.getInt("high"));
+                            Log.d(TAG, "low : " + jsonobj.getInt("low"));
+                            Log.d(TAG, "open : " + jsonobj.getInt("open"));
+                            Log.d(TAG, "pairVolume : " + jsonobj.getInt("pairVolume"));
+                            Log.d(TAG, "trades : " + jsonobj.getInt("trades"));
+                            Log.d(TAG, "volume : " + jsonobj.getInt("volume"));
                         }
+
+                            /*JSONArray names = response.names();
+                            JSONArray values = response.toJSONArray(names);
+                            for(int i=0; i<values.length(); i++){
+                                if (names.getString(i).equals("time")){
+                                    Log.d(TAG, "time : " + values.getString(i));
+                                }
+                                else if (names.getString(i).equals("close")){
+                                    Log.d(TAG, "close : " + values.getInt(i));
+                                }
+                                else if (names.getString(i).equals("high")){
+                                    Log.d(TAG, "high : " + values.getInt(i));
+                                }
+                                else if (names.getString(i).equals("low")){
+                                    Log.d(TAG, "low : " + values.getInt(i));
+                                }
+                                else if (names.getString(i).equals("open")){
+                                    Log.d(TAG, "open : " + values.getInt(i));
+                                }
+                                else if (names.getString(i).equals("pairVolume")){
+                                    Log.d(TAG, "pairVolume : " + values.getInt(i));
+                                }
+                                else if (names.getString(i).equals("trades")){
+                                    Log.d(TAG, "trades : " + values.getInt(i));
+                                }
+                                else if (names.getString(i).equals("volume")){
+                                    Log.d(TAG, "volume : " + values.getInt(i));
+                                }
+                            }*/
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                }, new Response.ErrorListener() {
+                }
+            }, new Response.ErrorListener() {
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        error.getMessage();
-                        Log.d(TAG, "Error : " + error.getMessage());
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // TODO Auto-generated method stub
+                    error.getMessage();
+                    Log.d(TAG, "Error : " + error.getMessage());
 
-                    }
-                });
+                }
+            });
 
-        // Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequestExchanges);
-    }
+    // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(
+
+    getApplicationContext()).
+
+    addToRequestQueue(jsObjRequestExchanges);
+}
 
 }
